9a36b8b1c693a23fd04d67b29d833febb7e25c38
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 17:11:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Extras for video creation things.'
 
 # Setting up
@@ -11,7 +11,7 @@ setup(
     author_email="<justcow@pm.me>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=['moviepy', 'yt_dlp', 'requests', 'oauth2client', 'google-api-python-client', 'google-auth-oauthlib', 'google-auth-httplib2'],
+    install_requires=['musicvideos', 'moviepy', 'yt_dlp', 'requests', 'oauth2client', 'google-api-python-client', 'google-auth-oauthlib', 'google-auth-httplib2'],
     keywords=['python', 'musicvideos', 'extras', 'youtube', 'justcow'],
     include_package_data=True ,
     classifiers=[
